6f79522ea158c53027a51324128d584eaff683cf
Marcus Canul <94863812+MeMarcusC@users.noreply.github.com>
2022-11-18 10:38:02 -0600

    Cookies-Darkmode

--- e-commerce/script.js
+++ e-commerce/script.js
@@ -15,14 +15,93 @@ if (close) {
     })
 }
 
+/* cookies */
 
-icon.onclick = function(){
-    document.body.classList.toggle("dark-theme");
-    if(document.body.classList.contains("dark-theme")){
-        icon.src = "img/sun.png";
-    }else{
-        icon.src = "img/moon.png";
-    }
+getCookie = (cName)=>{/* retrieve cookies value*/
+    const name = cName +"=";
+    const cDecoded = decodeURIComponent(document.cookie);
+    const cArr = cDecoded.split(";");
+    let value;
+    cArr.forEach(val =>{
+        if(val.indexOf(name)=== 0)value = val.substring(name.length);
+    })
+    return value;
 }
 
+cookieMessage = () => {/* will remov the cookie message if enabled */
+    if(!getCookie("cookie"))
+    {
+  document.querySelector("#cookies").style.display = "block";
+
+}}
+
+window.addEventListener("load",cookieMessage);//checks for cookies at page load
+
+setCookie = (cName, cValue, expDays) => {//cookie constructor
+    let date = new Date();
+    date.setTime(date.getTime() + (expDays * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = cName + "=" + cValue + "; " + expDays + date;
+}
+
+document.querySelector("#cookies-btn").addEventListener('click', () => {// create cookie when cookie button is clicked
+    document.querySelector("#cookies").style.display = "none";
+    setCookie("cookie",true,30);
+})
+function eraseCookie(name) {   //cookie destructor
+    document.cookie = name+'=; Max-Age=-99999999;';  
+}
+
+/* dark-theme*/
+
+function darkmode_init()
+{
+
+    
+    let darkmodeCookie = {
+        set:function(key,value,time,path,secure=false)
+        {
+            let expires = new Date();
+            expires.setTime(expires.getTime() + time);
+            var path   = (typeof path !== 'undefined') ? pathValue = 'path=' + path + ';' : '';
+            var secure = (secure) ? ';secure' : '';
+            
+            document.cookie = key + '=' + value + ';' + path + 'expires=' + expires.toUTCString() + secure;
+        },
+        get:function()
+        {
+            let keyValue = document.cookie.match('(^|;) ?darkmode=([^;]*)(;|$)');
+            return keyValue ? keyValue[2] : null;
+        },
+        remove:function()
+        {
+            document.cookie = 'darkmode=; Max-Age=0; path=/';
+        }
+    };
+    
+    
+    if(darkmodeCookie.get() == 'true')
+    {
+        icon.src = "img/sun.png";
+        document.body.classList.toggle('dark-theme');
+    }
+    
+    
+    icon.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        document.body.classList.toggle('dark-theme');
+        
+        if(document.body.classList.contains('dark-theme'))
+        {
+            icon.src = "img/sun.png";
+            darkmodeCookie.set('darkmode','true',2628000000,'/',false);
+        }
+        else
+        {
+            icon.src = "img/moon.png";
+            darkmodeCookie.remove();
+        }//
+    });
+}
 
